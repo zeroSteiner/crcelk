3a1f461193c7454aa9f63e55416b52e72136bd87
Spencer McIntyre <zeroSteiner@gmail.com>
2016-02-11 17:27:44 -0500

    Initial commit of CrcElk the CrcMoose fork

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# vim: tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab
+#
+# Copyright (C) 2005, 2007  Ray Burr
+# Copyright (C) 2016        Spencer McIntyre
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+#  Original (Moose) Homepage: http://www.nightmare.com/~ryb/
+#  New (Elk) Homepage: https://github.com/zeroSteiner/crcelk/
+
+from distutils.core import setup
+
+from crcelk import __version__
+
+setup(
+	name='CrcElk',
+	version=__version__,
+	author='Ray Burr',
+	maintainer='Spencer McIntyre',
+	description='Updated fork of CrcMoose.',
+	url='https://github.com/zeroSteiner/crcelk',
+	license='MIT',
+	py_modules=('crcelk',),
+	classifiers=(
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	)
+)
