d9415fa93006f788a27d309442155befc4b8395a
Spencer McIntyre <zeroSteiner@gmail.com>
2018-11-10 12:45:54 -0500

    Update setup.py whitespace and Python versions

--- setup.py
+++ setup.py
@@ -31,27 +31,29 @@ from distutils.core import setup
 from crcelk import __version__
 
 setup(
-	name='crcelk',
-	version=__version__,
-	author='Ray Burr',
-	maintainer='Spencer McIntyre',
-	description='Updated fork of CrcMoose.',
-	url='https://github.com/zeroSteiner/crcelk',
-	license='MIT',
-	py_modules=['crcelk',],
-	classifiers=[
-		'Development Status :: 5 - Production/Stable',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.1',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Topic :: Software Development :: Libraries :: Python Modules'
-	]
+    name='crcelk',
+    version=__version__,
+    author='Ray Burr',
+    maintainer='Spencer McIntyre',
+    description='Updated fork of CrcMoose.',
+    url='https://github.com/zeroSteiner/crcelk',
+    license='MIT',
+    py_modules=['crcelk',],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
